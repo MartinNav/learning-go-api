373be49921988875a4834504a82da40d0194fbc3
MartinNav <martinnavratilu@gmail.com>
2023-08-20 21:05:04 +0200

    Added user data parameter handling

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
-import(
-  //"fmt"
-  "log"
-  "net/http"
-  "encoding/json"
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 type BasicData struct{
   Lang string `json:"Lang"`
   IntVal int32 `json:"numeric"`
   RandomText string `json:"rtext"`
 }
+type ClientData struct{
+  Id uint32 `json:"id"`
+  Desc string `json:"desc"`
+}
+func jsonDataCall(w http.ResponseWriter, r *http.Request){
+  var cd ClientData
+  err := json.NewDecoder(r.Body).Decode(&cd)
+  if err != nil{
+    http.Error(w , err.Error(), http.StatusBadRequest)
+    return
+  }
+  fmt.Fprint(w , "Recieved: ",cd)
+  if cd.Id == 42{
+    fmt.Fprint(w ,"\nYou found the he meaning of life, the universe, and everything")
+  }
+}
+
 func basicData(w http.ResponseWriter, r *http.Request){
   data := BasicData{Lang: "Go",IntVal: 32,RandomText: "Idk somethink"}
 //  fmt.Fprintf(w,"Just hited the basicData endpoint")
@@ -19,6 +37,7 @@ func basicData(w http.ResponseWriter, r *http.Request){
 
 func handleRequests(){
   http.HandleFunc("/basic-data",basicData)
+  http.HandleFunc("/sdata", jsonDataCall)
   log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
